2410036d802b6d3e55745feafdbc5c38c5a72454
lamalgopach <malg.pachocki@gmail.com>
2018-10-10 00:07:06 +0000

    Second attempt to the lemmings.

---
+++ lemmings2.py
@@ -0,0 +1,60 @@
+"""Lazy lemmings.
+
+Find the farthest any single lemming needs to travel for food.
+
+    >>> furthest(3, [0, 1, 2])
+    0
+
+    >>> furthest(3, [2])
+    2
+
+    >>> furthest(3, [0])
+    2
+
+    >>> furthest(6, [2, 4])
+    2
+
+    >>> furthest(7, [0, 6])
+    3
+"""
+
+    # >>> furthest_optimized(7, [0, 6])
+    # 3
+
+    # >>> furthest_optimized(3, [0, 1, 2])
+    # 0
+
+    # >>> furthest_optimized(3, [2])
+    # 2
+
+    # >>> furthest_optimized(3, [0])
+    # 2
+
+    # >>> furthest_optimized(6, [2, 4])
+    # 2
+
+
+
+def furthest(num_holes, cafes):
+    """Find longest distance between a hole and a cafe."""
+
+    if num_holes == len(cafes):
+        return 0
+    worst = 0
+    for hole in range(num_holes):
+        dist = min(abs(hole - cafe)for cafe in cafes)
+        worst = max(worst, dist)
+    return worst
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print("\n*** ALL TESTS PASSED; GREAT JOB!\n")
